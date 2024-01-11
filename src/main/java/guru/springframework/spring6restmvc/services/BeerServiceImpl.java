ef0f6fc6075fc56c70c608581898fe7eea45d2ba
sorenara <fusanfilm@163.com>
2024-01-11 23:05:26 +0800

    adding http delete

--- src/main/java/guru/springframework/spring6restmvc/services/BeerServiceImpl.java
+++ src/main/java/guru/springframework/spring6restmvc/services/BeerServiceImpl.java
@@ -99,4 +99,11 @@ public class BeerServiceImpl implements BeerService {
 
         beerMap.put(existingBeer.getId(),existingBeer);
     }
+
+    @Override
+    public void deleteById(UUID beerId) {
+        beerMap.remove(beerId);
+    }
+
+
 }
