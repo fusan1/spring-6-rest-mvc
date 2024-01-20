0d422ea6cd1db7f2e41bcb077b42137c0650e487
sorenara <fusanfilm@163.com>
2024-01-20 13:03:32 +0800

    adding entities

---
+++ src/main/java/guru/springframework/spring6restmvc/entities/Customer.java
@@ -0,0 +1,25 @@
+package guru.springframework.spring6restmvc.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Version;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Customer {
+    private String name;
+    @Id
+    private UUID id;
+    @Version
+    private Integer version;
+    private LocalDateTime createdDate;
+    private LocalDateTime updateDate;
+}
