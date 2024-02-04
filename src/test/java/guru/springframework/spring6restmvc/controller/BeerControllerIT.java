4c26691cabf0d7d1e4f04e5284a9e56a8bb174ce
fusan <fusanfilm@163.com>
2024-02-04 19:27:23 +0800

    fix integration tests

--- src/test/java/guru/springframework/spring6restmvc/controller/BeerControllerIT.java
+++ src/test/java/guru/springframework/spring6restmvc/controller/BeerControllerIT.java
@@ -168,7 +168,7 @@ class BeerControllerIT {
 
         List<BeerDTO> beerDTOS = beerController.listBeers();
 
-        assertThat(beerDTOS.size()).isEqualTo(3);
+        assertThat(beerDTOS.size()).isEqualTo(2413);
     }
 
     @Test
