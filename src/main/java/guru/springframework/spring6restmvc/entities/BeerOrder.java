5a6b8c20d97df62e118610d417fb94b98c3cbd21
fusan <fusanfilm@163.com>
2024-02-11 12:25:04 +0800

    rel-assn-one-to-many

--- src/main/java/guru/springframework/spring6restmvc/entities/BeerOrder.java
+++ src/main/java/guru/springframework/spring6restmvc/entities/BeerOrder.java
@@ -9,6 +9,7 @@ import org.hibernate.annotations.UpdateTimestamp;
 import org.hibernate.type.SqlTypes;
 
 import java.sql.Timestamp;
+import java.util.Set;
 import java.util.UUID;
 @Getter
 @Setter
@@ -45,5 +46,9 @@ public class BeerOrder {
 
     @ManyToOne
     private Customer customer;
+
+    @OneToMany(mappedBy = "beerOrder")
+    private Set<BeerOrderLine> beerOrderLines;
+
 
 }
