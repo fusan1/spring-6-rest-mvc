8857746e1997242294d66aabd00bee7ff3c5ea6e
fusan <fusanfilm@163.com>
2024-03-01 20:06:29 +0800

    adding SpringSecConfig

---
+++ src/test/java/guru/springframework/spring6restmvc/config/SpringSecConfig.java
@@ -0,0 +1,17 @@
+package guru.springframework.spring6restmvc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SpringSecConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf().ignoringRequestMatchers("/api/**");
+        return httpSecurity.build();
+
+    }
+}
